628593a7a7b07467f6a66d85260840dc6ff80b0d
Shabbir Ansari <sansari@ariqt.com>
2023-01-11 19:49:43 +0530

    Initial commit

---
+++ Test 2/JwtAuthentication_In_ThreeTierArchitecture/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JwtAuthentication_In_ThreeTierArchitecture.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        // GET: api/<EmployeeController>
+        [HttpGet("GetListById")]
+        public async Task<ActionResult> GetListById()
+        {
+            return Ok();
+        }
+        [HttpGet("Get")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Vinay", "Ravula" };
+        }
+    }
+}
